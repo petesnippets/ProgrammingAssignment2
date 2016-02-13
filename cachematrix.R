75b95c065c70780b41ed4dd35120a8c39d885dd3
pete <pete@searchahead.co.uk>
2016-02-13 18:16:29 +0000

    Adding example usage

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,19 @@
 ## This file contains the solution to programming assignment 2
 
+## Example usage
+## Assuming a matrix mtx exists
+
+##cm = makeCacheMatrix(mtx)
+##inv <- cacheSolve(cm)
+## calling inv <- cacheSolve(cm) a second time will retrieve the inverse from the cache
+## and print the message 'returning inverse from the cache'
+## the inverse result can be tested by entering round(inv %*% mtx)
+## this returns the identity matrix as follows
+##     [,1] [,2] [,3]
+##[1,]    1    0    0
+##[2,]    0    1    0
+##[3,]    0    0    1
+
 
 ## returns a list containing:
 ## functions to get and set the matrix
